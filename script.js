a881bb2c023021a95ce4ffaea09aa92551e53d2d
Tushar-Mehra1707 <tusharmehra4321@gmail.com>
2025-04-29 14:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,64 @@
-//complete this code
+
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(value) {
+    this._age = value;
+  }
+}
+
+// Subclass: Student
+class Student extends Person {
+  study() {
+    log(`${this.name} is studying`);
+  }
+}
+
+// Subclass: Teacher
+class Teacher extends Person {
+  teach() {
+    log(`${this.name} is teaching`);
+  }
+}
+
+// Utility function to output to the screen
+function log(message) {
+  const output = document.getElementById('output');
+  output.textContent += message + '\n';
+}
+
+// Testing functions
+function testPerson() {
+  const person = new Person("John", 25);
+  log(`Name: ${person.name}`); // Getter
+  person.age = 30;             // Setter
+  log(`Updated Age: ${person.age}`);
+}
+
+function testStudent() {
+  const student = new Student("Alice", 22);
+  student.study(); // Should log "Alice is studying"
+}
+
+function testTeacher() {
+  const teacher = new Teacher("Bob", 40);
+  teacher.teach(); // Should log "Bob is teaching"
+}
+
 class Person {}
 
 class Student extends Person {}
